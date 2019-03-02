7b57eb9047f8695908e3b738f8f65f939393b05b
Jason Bukowski <jbukowski@gmail.com>
2019-03-02 10:08:32 -0500

    Added RocksDB methods for storing, retrieving, and pruning proofs

---
+++ node-lib/lib/models/RocksDB.js
@@ -0,0 +1,209 @@
+/**
+ * Copyright 2019 Tierion
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+let env = require('../parse-env.js').env
+const level = require('level-rocksdb')
+const crypto = require('crypto')
+const path = require('path')
+
+// See Options: https://github.com/level/leveldown#options
+// Setup with options, all default except:
+//   cacheSize : which was increased from 8MB to 32MB
+let options = {
+  createIfMissing: true,
+  errorIfExists: false,
+  compression: true,
+  cacheSize: 32 * 1024 * 1024,
+  writeBufferSize: 4 * 1024 * 1024,
+  blockSize: 4096,
+  maxOpenFiles: 1000,
+  blockRestartInterval: 16,
+  maxFileSize: 2 * 1024 * 1024,
+  keyEncoding: 'binary',
+  valueEncoding: 'binary'
+}
+
+const prefixBuffers = {
+  PROOF_TIME_INDEX: Buffer.from('b1a1', 'hex'),
+  PROOF_CONTENT: Buffer.from('b1a2', 'hex')
+}
+
+const PRUNE_BATCH_SIZE = 1000
+const PRUNE_INTERVAL_SECONDS = 10
+let PRUNE_IN_PROGRESS = false
+
+let db = null
+
+async function openConnectionAsync(dir = '../../data/rocksdb') {
+  return new Promise(resolve => {
+    level(path.resolve(dir), options, (err, conn) => {
+      if (err) {
+        console.error(`ERROR : Unable to open database : ${err.message}`)
+        process.exit(0)
+      } else {
+        db = conn
+        resolve(db)
+      }
+    })
+  })
+}
+
+// #endregion SCHEMAS
+
+/****************************************************************************************************
+ * PROOF FUNCTIONS
+ ****************************************************************************************************/
+// #region PROOF FUNCTIONS
+
+function createBinaryProofContentKey(hashId) {
+  let uuidBuffer = Buffer.from(hashId.replace(/-/g, ''), 'hex')
+  return Buffer.concat([prefixBuffers.PROOF_CONTENT, uuidBuffer])
+}
+
+function createBinaryProofTimeIndexKey() {
+  // generate a new key for the current time
+  let timestampBuffer = Buffer.alloc(8)
+  timestampBuffer.writeDoubleBE(Date.now())
+  let rndBuffer = crypto.randomBytes(16)
+  return Buffer.concat([prefixBuffers.PROOF_TIME_INDEX, timestampBuffer, rndBuffer])
+}
+
+function createBinaryProofTimeIndexMin() {
+  // generate the minimum key value for range query
+  let minBoundsBuffer = Buffer.alloc(24, 0)
+  return Buffer.concat([prefixBuffers.PROOF_TIME_INDEX, minBoundsBuffer])
+}
+
+function createBinaryProofTimeIndexMax(timestamp) {
+  // generate the maximum key value for range query up to given timestamp
+  let timestampBuffer = Buffer.alloc(8, 0)
+  timestampBuffer.writeDoubleBE(timestamp)
+  let rndBuffer = Buffer.alloc(16, 'ff', 'hex')
+  return Buffer.concat([prefixBuffers.PROOF_TIME_INDEX, timestampBuffer, rndBuffer])
+}
+
+async function getProofBatchByHashIdsAsync(hashIds) {
+  let results = []
+  for (let hashId of hashIds) {
+    try {
+      let proofContentKey = createBinaryProofContentKey(hashId)
+      let proofContent = await db.get(proofContentKey)
+      let coreProof = JSON.parse(proofContent)
+      results.push({
+        hashId: hashId,
+        proof: coreProof
+      })
+    } catch (error) {
+      if (error.notFound) {
+        results.push({
+          hashId: hashId,
+          proof: null
+        })
+      } else {
+        let err = `Unable to read proof for hash with hashId = ${hashId} : ${error.message}`
+        throw err
+      }
+    }
+  }
+  return results
+}
+
+async function saveProofBatchAsync(proofs) {
+  let ops = []
+
+  for (let proof of proofs) {
+    let proofContentKey = createBinaryProofContentKey(proof.hash_id_core)
+    let proofTimeIndexKey = createBinaryProofTimeIndexKey()
+    let proofContent = JSON.stringify(proof)
+    ops.push({ type: 'put', key: proofContentKey, value: proofContent })
+    ops.push({ type: 'put', key: proofTimeIndexKey, value: proofContentKey })
+  }
+
+  try {
+    await db.batch(ops)
+  } catch (error) {
+    let err = `Unable to write proofs : ${error.message}`
+    throw err
+  }
+}
+
+async function pruneProofsSince(timestampMS) {
+  return new Promise((resolve, reject) => {
+    let delOps = []
+    let minKey = createBinaryProofTimeIndexMin()
+    let maxKey = createBinaryProofTimeIndexMax(timestampMS)
+    db.createReadStream({ gt: minKey, lte: maxKey })
+      .on('data', async data => {
+        delOps.push({ type: 'del', key: data.key })
+        delOps.push({ type: 'del', key: data.value })
+        // Execute in batches of PRUNE_BATCH_SIZE
+        if (delOps.length >= PRUNE_BATCH_SIZE) {
+          try {
+            let delOpsBatch = delOps.splice(0)
+            await db.batch(delOpsBatch)
+          } catch (error) {
+            let err = `Error during proof batch delete : ${error.message}`
+            return reject(err)
+          }
+        }
+      })
+      .on('error', error => {
+        let err = `Error reading proof keys for pruning : ${error.message}`
+        return reject(err)
+      })
+      .on('end', async () => {
+        try {
+          await db.batch(delOps)
+        } catch (error) {
+          return reject(error.message)
+        }
+        return resolve()
+      })
+  })
+}
+
+async function pruneOldProofsAsync() {
+  let pruneTime = Date.now() - env.PROOF_EXPIRE_MINUTES * 60 * 1000
+  try {
+    await pruneProofsSince(pruneTime)
+  } catch (error) {
+    console.error(`ERROR : An error occurred during proof pruning : ${error.message}`)
+  }
+}
+
+// #endregion PROOF FUNCTIONS
+
+/****************************************************************************************************
+ * SET AUTOMATIC PRUNING INTERVALS
+ ****************************************************************************************************/
+// #region SET AUTOMATIC PRUNING INTERVALS
+
+function startPruningInterval() {
+  return setInterval(async () => {
+    if (!PRUNE_IN_PROGRESS) {
+      PRUNE_IN_PROGRESS = true
+      await pruneOldProofsAsync()
+      PRUNE_IN_PROGRESS = false
+    }
+  }, PRUNE_INTERVAL_SECONDS * 1000)
+}
+
+// #endregion SET AUTOMATIC PRUNING INTERVALS
+
+module.exports = {
+  openConnectionAsync: openConnectionAsync,
+  getProofBatchByHashIdsAsync: getProofBatchByHashIdsAsync,
+  saveProofBatchAsync: saveProofBatchAsync,
+  startPruningInterval: startPruningInterval,
+  pruneOldProofsAsync: pruneOldProofsAsync
+}
