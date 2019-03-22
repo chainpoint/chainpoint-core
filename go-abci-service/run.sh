39cc12ff5a4d04fddefd727994fb3c6440ce0f87
jacohend <jacob@tierion.com>
2019-03-21 17:09:28 -0700

    add docker secrets support shim to Dockerfiles

---
+++ go-abci-service/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source ./env_secrets_expand.sh
+tendermint node $PEERS --moniker=`hostname` --proxy_app=abci:26658
---
+++ node-btc-tx-service/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source ./env_secrets_expand.sh
+yarn start
