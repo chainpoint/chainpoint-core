326f14343e3eea4fe389649378478de5db7fd8a0
Jason Bukowski <jbukowski@gmail.com>
2019-04-04 15:03:05 -0400

    Updated copyright

--- node-lib/lib/models/StakedNode.js
+++ node-lib/lib/models/StakedNode.js
@@ -1,5 +1,5 @@
 
-/* Copyright (C) 2018 Tierion
+/* Copyright (C) 2019 Tierion
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
