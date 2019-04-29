0f4f04e88b47b9ad0e2ccc46011ebdce7aa902bf
Jason Bukowski <jbukowski@gmail.com>
2019-04-29 10:40:53 -0400

    Added root tests

---
+++ node-api-service/test/root.js
@@ -0,0 +1,41 @@
+/* global describe, it, beforeEach, afterEach */
+
+process.env.NODE_ENV = 'test'
+
+// test related packages
+const expect = require('chai').expect
+const request = require('supertest')
+
+const app = require('../server.js')
+
+describe('Root Controller', () => {
+  let insecureServer = null
+  beforeEach(async () => {
+    app.setThrottle(() => (req, res, next) => next())
+    insecureServer = await app.startInsecureRestifyServerAsync()
+  })
+  afterEach(() => {
+    insecureServer.close()
+  })
+
+  describe('GET /', () => {
+    it('should return teapot error', done => {
+      request(insecureServer)
+        .get('/')
+        .expect('Content-type', /json/)
+        .expect(418)
+        .end((err, res) => {
+          expect(err).to.equal(null)
+          expect(res.body)
+            .to.have.property('code')
+            .and.to.be.a('string')
+            .and.to.equal('ImATeapot')
+          expect(res.body)
+            .to.have.property('message')
+            .and.to.be.a('string')
+            .and.to.equal('This is an API endpoint. Please consult https://chainpoint.org')
+          done()
+        })
+    })
+  })
+})
