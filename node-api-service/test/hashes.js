710ac825a7cdd6325b7691f598f14a5b8e58045c
Jason Bukowski <jbukowski@gmail.com>
2019-05-01 18:09:44 -0400

    Updated existing hashes tests to use JWTs as needed

--- node-api-service/test/hashes.js
+++ node-api-service/test/hashes.js
@@ -1,4 +1,4 @@
-/* global describe, it, beforeEach, afterEach */
+/* global describe, it, before, beforeEach, afterEach */
 
 process.env.NODE_ENV = 'test'
 
@@ -134,6 +134,26 @@ describe('Hashes Controller', () => {
   })
 
   describe('POST /hashes', () => {
+    let cache = null
+    let jwk = {
+      kty: 'EC',
+      kid: 'OEQiv4MMu9pf3MK1DcIMczWqd3nBjyKvOGjLCUurs4E',
+      crv: 'P-256',
+      x: 'E1tQ_do30mgWfcj2zhnpTFx7FfxSJ6S6gl6R8VaIuBk',
+      y: 'ezM2bavf93gkFH3bm9oChRtFX3tm4WuwdIb43ukvwlY'
+    }
+    let jwkStr = JSON.stringify(jwk)
+    before(() => {
+      hashes.setRedis({
+        get: async () => null,
+        set: async (k, v) => {
+          cache = [k, v]
+        }
+      })
+      hashes.setRP(async () => {
+        return { body: { jwk: jwk } }
+      })
+    })
     it('should return a matched set of metadata and UUID embedded timestamps', done => {
       app.setAMQPChannel({
         sendToQueue: function() {}
@@ -141,7 +161,11 @@ describe('Hashes Controller', () => {
 
       request(insecureServer)
         .post('/hashes')
-        .send({ hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12' })
+        .send({
+          hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12',
+          token:
+            'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9FUWl2NE1NdTlwZjNNSzFEY0lNY3pXcWQzbkJqeUt2T0dqTENVdXJzNEUifQ.eyJqdGkiOiI2MzFiYmYxMC02YzVhLTExZTktOTc3Ni04ZDJmNjcxNWYxNWMiLCJpc3MiOiJodHRwOi8vMzUuMjQ1LjIxMS45NyIsInN1YiI6IjI0LjE1NC4yMS4xMSIsImV4cCI6MTg3MjEwNzEwOCwiYmFsIjoxMCwiaWF0IjoxNTU2NzQ3MTA3fQ.3dJ47LQPnrFRZLT47e7sBe4qc3RX1AHWjaaPRCVLKKT5KWwvADmGY5_VB05i5xuBqc9sH_qUcnDaWY6cff48xg'
+        })
         .expect('Content-type', /json/)
         .expect(200)
         .end((err, res) => {
@@ -151,12 +175,36 @@ describe('Hashes Controller', () => {
           // The UUID timestamp has ms level precision, ISO8601 only to the second.
           // Check that they are within 1000ms of each other.
           expect(parseInt(uuidTime.v1(res.body.hash_id)) - Date.parse(res.body.submitted_at)).to.be.within(0, 1000)
+          expect(cache)
+            .to.be.a('array')
+            .and.to.have.length(2)
+          expect(cache[1]).to.equal(jwkStr)
           done()
         })
     })
   })
 
   describe('POST /hashes', () => {
+    let cache = null
+    let jwk = {
+      kty: 'EC',
+      kid: 'OEQiv4MMu9pf3MK1DcIMczWqd3nBjyKvOGjLCUurs4E',
+      crv: 'P-256',
+      x: 'E1tQ_do30mgWfcj2zhnpTFx7FfxSJ6S6gl6R8VaIuBk',
+      y: 'ezM2bavf93gkFH3bm9oChRtFX3tm4WuwdIb43ukvwlY'
+    }
+    let jwkStr = JSON.stringify(jwk)
+    before(() => {
+      hashes.setRedis({
+        get: async () => null,
+        set: async (k, v) => {
+          cache = [k, v]
+        }
+      })
+      hashes.setRP(async () => {
+        return { body: { jwk: jwk } }
+      })
+    })
     it('should return a v1 UUID node embedded with a partial SHA256 over timestamp and hash', done => {
       app.setAMQPChannel({
         sendToQueue: function() {}
@@ -164,7 +212,11 @@ describe('Hashes Controller', () => {
 
       request(insecureServer)
         .post('/hashes')
-        .send({ hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12' })
+        .send({
+          hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12',
+          token:
+            'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9FUWl2NE1NdTlwZjNNSzFEY0lNY3pXcWQzbkJqeUt2T0dqTENVdXJzNEUifQ.eyJqdGkiOiI2MzFiYmYxMC02YzVhLTExZTktOTc3Ni04ZDJmNjcxNWYxNWMiLCJpc3MiOiJodHRwOi8vMzUuMjQ1LjIxMS45NyIsInN1YiI6IjI0LjE1NC4yMS4xMSIsImV4cCI6MTg3MjEwNzEwOCwiYmFsIjoxMCwiaWF0IjoxNTU2NzQ3MTA3fQ.3dJ47LQPnrFRZLT47e7sBe4qc3RX1AHWjaaPRCVLKKT5KWwvADmGY5_VB05i5xuBqc9sH_qUcnDaWY6cff48xg'
+        })
         .expect('Content-type', /json/)
         .expect(200)
         .end((err, res) => {
@@ -185,12 +237,36 @@ describe('Hashes Controller', () => {
           let shortHashNodeBuf = Buffer.concat([Buffer.from([0x01]), h.digest()])
           // Last segment of UUIDv1 contains BLAKE2s hash to be matched
           expect(res.body.hash_id.split('-')[4]).to.equal(shortHashNodeBuf.toString('hex'))
+          expect(cache)
+            .to.be.a('array')
+            .and.to.have.length(2)
+          expect(cache[1]).to.equal(jwkStr)
           done()
         })
     })
   })
 
   describe('POST /hashes', () => {
+    let cache = null
+    let jwk = {
+      kty: 'EC',
+      kid: 'OEQiv4MMu9pf3MK1DcIMczWqd3nBjyKvOGjLCUurs4E',
+      crv: 'P-256',
+      x: 'E1tQ_do30mgWfcj2zhnpTFx7FfxSJ6S6gl6R8VaIuBk',
+      y: 'ezM2bavf93gkFH3bm9oChRtFX3tm4WuwdIb43ukvwlY'
+    }
+    let jwkStr = JSON.stringify(jwk)
+    before(() => {
+      hashes.setRedis({
+        get: async () => null,
+        set: async (k, v) => {
+          cache = [k, v]
+        }
+      })
+      hashes.setRP(async () => {
+        return { body: { jwk: jwk } }
+      })
+    })
     it('should return proper result with valid call', done => {
       app.setAMQPChannel({
         sendToQueue: function() {}
@@ -198,7 +274,11 @@ describe('Hashes Controller', () => {
 
       request(insecureServer)
         .post('/hashes')
-        .send({ hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12' })
+        .send({
+          hash: 'ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12ab12',
+          token:
+            'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9FUWl2NE1NdTlwZjNNSzFEY0lNY3pXcWQzbkJqeUt2T0dqTENVdXJzNEUifQ.eyJqdGkiOiI2MzFiYmYxMC02YzVhLTExZTktOTc3Ni04ZDJmNjcxNWYxNWMiLCJpc3MiOiJodHRwOi8vMzUuMjQ1LjIxMS45NyIsInN1YiI6IjI0LjE1NC4yMS4xMSIsImV4cCI6MTg3MjEwNzEwOCwiYmFsIjoxMCwiaWF0IjoxNTU2NzQ3MTA3fQ.3dJ47LQPnrFRZLT47e7sBe4qc3RX1AHWjaaPRCVLKKT5KWwvADmGY5_VB05i5xuBqc9sH_qUcnDaWY6cff48xg'
+        })
         .expect('Content-type', /json/)
         .expect(200)
         .end((err, res) => {
@@ -219,6 +299,10 @@ describe('Hashes Controller', () => {
           expect(res.body.processing_hints)
             .to.have.property('btc')
             .and.to.be.a('string')
+          expect(cache)
+            .to.be.a('array')
+            .and.to.have.length(2)
+          expect(cache[1]).to.equal(jwkStr)
           done()
         })
     })
