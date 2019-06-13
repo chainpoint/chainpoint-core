b183445824ff6b30f5995348a214039b3bf0e8d5
jacohend <jacob@tierion.com>
2019-06-13 12:29:26 -0700

    move btctx queue methods to anchor.go from monitor.go

--- go-abci-service/abci/anchor.go
+++ go-abci-service/abci/anchor.go
@@ -4,8 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/chainpoint/chainpoint-core/go-abci-service/rabbitmq"
+	"github.com/chainpoint/chainpoint-core/go-abci-service/util"
+	"github.com/streadway/amqp"
 
 	"github.com/chainpoint/chainpoint-core/go-abci-service/types"
 )
@@ -111,6 +116,163 @@ func (app *AnchorApplication) AnchorBTC(startTxRange int64, endTxRange int64) er
 		return nil
 	}
 	return errors.New("no transactions to aggregate")
+}
+
+// ConsumeBtcTxMsg : Consumes a btctx RMQ message to initiate monitoring on all nodes
+func (app *AnchorApplication) ConsumeBtcTxMsg(msgBytes []byte) error {
+	var btcTxObj types.BtcTxMsg
+	if err := json.Unmarshal(msgBytes, &btcTxObj); err != nil {
+		return app.LogError(err)
+	}
+	app.state.LatestBtcTx = btcTxObj.BtcTxID // Update app state with txID so we can broadcast BTC-A
+	stateObj := types.BtcTxProofState{
+		AnchorBtcAggID: btcTxObj.AnchorBtcAggID,
+		BtcTxID:        btcTxObj.BtcTxID,
+		BtcTxState: types.BtcTxOpsState{
+			Ops: []types.ProofLineItem{
+				types.ProofLineItem{
+					Left: btcTxObj.BtcTxBody[:strings.Index(btcTxObj.BtcTxBody, btcTxObj.AnchorBtcAggRoot)],
+				},
+				types.ProofLineItem{
+					Right: btcTxObj.BtcTxBody[strings.Index(btcTxObj.BtcTxBody, btcTxObj.AnchorBtcAggRoot)+len(btcTxObj.AnchorBtcAggRoot):],
+				},
+				types.ProofLineItem{
+					Op: "sha-256-x2",
+				},
+			},
+		},
+	}
+	dataJSON, err := json.Marshal(stateObj)
+	if app.LogError(err) != nil {
+		return err
+	}
+	err = rabbitmq.Publish(app.config.RabbitmqURI, "work.proofstate", "btctx", dataJSON)
+	if err != nil {
+		rabbitmq.LogError(err, "rmq dial failure, is rmq connected?")
+		return err
+	}
+	txIDBytes, err := json.Marshal(types.TxID{TxID: btcTxObj.BtcTxID})
+	err = rabbitmq.Publish(app.config.RabbitmqURI, "work.btcmon", "", txIDBytes)
+	if err != nil {
+		rabbitmq.LogError(err, "rmq dial failure, is rmq connected?")
+		return err
+	}
+	return nil
+}
+
+// ConsumeBtcMonMsg : consumes a btc mon message and issues a BTC-Confirm transaction along with completing btc proof generation
+func (app *AnchorApplication) ConsumeBtcMonMsg(msg amqp.Delivery) error {
+	var anchoringCoreID string
+	var hash []byte
+	var btcMonObj types.BtcMonMsg
+	util.LoggerError(app.logger, json.Unmarshal(msg.Body, &btcMonObj))
+	// Get the CoreID that originally published the anchor TX using the btc tx ID we tagged it with
+	txResult, err := app.rpc.client.TxSearch(fmt.Sprintf("BTCTX=%s", btcMonObj.BtcTxID), false, 1, 25)
+	app.LogError(err)
+	for _, tx := range txResult.Txs {
+		decoded, err := util.DecodeTx(tx.Tx)
+		if app.LogError(err) != nil {
+			continue
+		}
+		anchoringCoreID = decoded.CoreID
+	}
+	if len(anchoringCoreID) == 0 {
+		app.logger.Error(fmt.Sprintf("Anchor: Cannot retrieve BTCTX-tagged transaction for btc tx: %s", btcMonObj.BtcTxID))
+	}
+	// Broadcast the confirmation message with metadata
+	result, err := app.rpc.BroadcastTxWithMeta("BTC-C", btcMonObj.BtcHeadRoot, 2, time.Now().Unix(), app.ID, anchoringCoreID+"|"+btcMonObj.BtcTxID, &app.config.ECPrivateKey)
+	time.Sleep(1 * time.Minute) // wait until it hits the mempool
+	if app.LogError(err) != nil {
+		app.logger.Error(fmt.Sprintf("Anchor: Another core has probably already committed a BTCC tx: %s", err.Error()))
+		txResult, err := app.rpc.GetTxByInt(app.state.LatestBtccTxInt)
+		if app.LogError(err) != nil && len(txResult.Txs) > 0 {
+			hash = txResult.Txs[0].Hash
+		} else {
+			return err
+		}
+	} else {
+		hash = result.Hash
+	}
+	var btccStateObj types.BtccStateObj
+	btccStateObj.BtcTxID = btcMonObj.BtcTxID
+	btccStateObj.BtcHeadHeight = btcMonObj.BtcHeadHeight
+	btccStateObj.BtcHeadState.Ops = make([]types.ProofLineItem, 0)
+	for _, p := range btcMonObj.Path {
+		if p.Left != "" {
+			btccStateObj.BtcHeadState.Ops = append(btccStateObj.BtcHeadState.Ops, types.ProofLineItem{Left: string(p.Left)})
+		}
+		if p.Right != "" {
+			btccStateObj.BtcHeadState.Ops = append(btccStateObj.BtcHeadState.Ops, types.ProofLineItem{Right: string(p.Right)})
+		}
+		btccStateObj.BtcHeadState.Ops = append(btccStateObj.BtcHeadState.Ops, types.ProofLineItem{Op: "sha-256-x2"})
+	}
+	baseURI := util.GetEnv("CHAINPOINT_CORE_BASE_URI", "https://tendermint.chainpoint.org")
+	uri := strings.ToLower(fmt.Sprintf("%s/calendar/%x/data", baseURI, hash))
+	btccStateObj.BtcHeadState.Anchor = types.AnchorObj{
+		AnchorID: strconv.FormatInt(btcMonObj.BtcHeadHeight, 10),
+		Uris:     []string{uri},
+	}
+	stateObjBytes, err := json.Marshal(btccStateObj)
+
+	err = rabbitmq.Publish(app.config.RabbitmqURI, "work.proofstate", "btcmon", stateObjBytes)
+	if err != nil {
+		rabbitmq.LogError(err, "rmq dial failure, is rmq connected?")
+		return err
+	}
+	msg.Ack(false)
+	return nil
+}
+
+func (app *AnchorApplication) processMessage(msg amqp.Delivery) error {
+	switch msg.Type {
+	case "btctx":
+		time.Sleep(30 * time.Second)
+		_, err := app.rpc.BroadcastTx("BTC-M", string(msg.Body), 2, time.Now().Unix(), app.ID, &app.config.ECPrivateKey)
+		if app.LogError(err) != nil {
+			return err
+		}
+		msg.Ack(false)
+		break
+	case "btcmon":
+		err := app.ConsumeBtcMonMsg(msg)
+		app.LogError(err)
+		break
+	case "reward":
+		break
+	default:
+		msg.Ack(false)
+	}
+	return nil
+}
+
+// ReceiveCalRMQ : Continually consume the calendar work queue and
+// process any resulting messages from the tx and monitor services
+func (app *AnchorApplication) ReceiveCalRMQ() error {
+	var session rabbitmq.Session
+	var err error
+	endConsume := false
+	for {
+		session, err = rabbitmq.ConnectAndConsume(app.config.RabbitmqURI, "work.cal")
+		if err != nil {
+			rabbitmq.LogError(err, "failed to dial for work.cal queue")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		for {
+			select {
+			case err = <-session.Notify:
+				if endConsume {
+					return err
+				}
+				time.Sleep(5 * time.Second)
+				break //reconnect
+			case msg := <-session.Msgs:
+				if len(msg.Body) > 0 {
+					go app.processMessage(msg)
+				}
+			}
+		}
+	}
 }
 
 // resetAnchor ensures that anchoring will begin again in the next block
