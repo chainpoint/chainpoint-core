5731978899965eede37115a471840e30cb213cd3
Jason Bukowski <jbukowski@gmail.com>
2019-08-07 16:30:45 -0400

    Removed ETH related packages

--- cli/scripts/0_swarm_secrets.js
+++ cli/scripts/0_swarm_secrets.js
@@ -16,11 +16,7 @@
 
 const exec = require('executive')
 const chalk = require('chalk')
-const keccak = require('keccak')
-const secp256k1 = require('secp256k1')
-const createWallet = require('./1_create_wallet')
 const updateOrCreateEnv = require('./2_update_env')
-const displayInfo = require('./1b_display_info')
 
 async function createSwarmAndSecrets(valuePairs) {
   let home = await exec.quiet('/bin/bash -c "$(eval printf ~$USER)"')
@@ -41,39 +37,11 @@ async function createSwarmAndSecrets(valuePairs) {
     await exec([
       sed, //sed line needs to be first for some reason
       `docker swarm init --advertise-addr=${ip} || echo "Swarm already initialized"`,
-      `openssl ecparam -genkey -name secp256r1 -noout -out ${home.stdout}/.chainpoint/core/data/keys/ecdsa_key.pem`,
-      `cat ${home.stdout}/.chainpoint/core/data/keys/ecdsa_key.pem | docker secret create ECDSA_PKPEM -`,
       `printf ${wif} | docker secret create BITCOIN_WIF -`
     ])
     console.log(chalk.yellow('Secrets saved to Docker Secrets'))
   } catch (err) {
     console.log(chalk.red('Setting secrets failed (is docker installed?)'))
-  }
-
-  try {
-    let privateKey
-    if (!('ETH_PRIVATE_KEY' in valuePairs)) {
-      privateKey = (await createWallet()).privateKey
-    } else {
-      privateKey = valuePairs.ETH_PRIVATE_KEY
-    }
-    /* Derive address from private key */
-    let privateKeyBytes = new Buffer(privateKey.slice(2), 'hex')
-    let pubKey = secp256k1.publicKeyCreate(privateKeyBytes, false).slice(1)
-    let address =
-      '0x' +
-      keccak('keccak256')
-        .update(pubKey)
-        .digest()
-        .slice(-20)
-        .toString('hex')
-    await exec.quiet([
-      `printf ${address} | docker secret create ETH_ADDRESS -`,
-      `printf ${privateKey} | docker secret create ETH_PRIVATE_KEY -`
-    ])
-    await displayInfo.displayWalletInfo({ address: address, privateKey: privateKey })
-  } catch (err) {
-    console.log(chalk.red(`Error creating Docker secrets for ETH_ADDRESS & ETH_PRIVATE_KEY: ${err}`))
   }
 
   return updateOrCreateEnv({
