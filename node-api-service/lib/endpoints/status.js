7ad35f95fa726d2686ce9b325776f59a01c2292b
jacohend <jacob@tierion.com>
2019-09-03 12:50:08 -0700

    add error logging, switch to 'next' branch of node-lnd-grpc

--- node-api-service/lib/endpoints/status.js
+++ node-api-service/lib/endpoints/status.js
@@ -67,6 +67,7 @@ async function buildStatusObjectAsync() {
   try {
     walletInfo = await lnService.getWalletInfo({ lnd })
   } catch (error) {
+    logger.error(`GRPC error communicating with LND : ${statusResponse.error.message}`)
     return { status: null, errorCode: 500, errorMessage: 'Could not query for status' }
   }
 
--- node-lnd-mon-service/server.js
+++ node-lnd-mon-service/server.js
@@ -21,7 +21,6 @@ const connections = require('./lib/connections.js')
 const logger = require('./lib/logger.js')
 const utils = require('./lib/utils.js')
 const LndGrpc = require('lnd-grpc')
-const lightning = require('lnrpc-node-client')
 
 const LAST_KNOWN_INVOICE_INDEX_KEY = 'LastKnownInvoiceIndex'
 const INVOICE_BATCH_SIZE = 1000
@@ -55,7 +54,6 @@ let lnd = new LndGrpc({
   cert: `/root/.lnd/tls.cert`,
   macaroon: `/root/.lnd/data/chain/bitcoin/${env.NETWORK}/admin.macaroon`
 })
-lightning.setTls(env.LND_SOCKET, '/root/.lnd/tls.cert')
 
 async function processInvoiceBatchAsync(invoices) {
   let invoiceRedisOps = invoices
@@ -91,12 +89,24 @@ async function processInvoiceBatchAsync(invoices) {
 }
 
 async function connectToLndAsync() {
-  lightning.unlocker().unlockWallet({ wallet_password: env.HOT_WALLET_PASS }, (err, res) => {
-    console.log(res)
-    console.log(err)
-  })
+  try {
+    await lnd.disconnect()
+  } catch (error) {
+    throw new Error(`LND disconnect failed: ${error.message}`)
+  }
   try {
     await lnd.connect()
+    try {
+      await lnd.services.WalletUnlocker.unlockWallet({
+        wallet_password: Buffer.from(env.HOT_WALLET_PASS)
+      })
+    } catch (error) {
+      throw new Error(`Can't unlocked LND, already unlocked? : ${error.message}`)
+    }
+    await lnd.activateLightning()
+    lnd.once('active', async () => {
+      logger.info('GRPC state active')
+    })
   } catch (error) {
     throw new Error(`Unable to connect to LND : ${error.message}`)
   }
