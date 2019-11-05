d4d06d062983fbda3ecb66dea9539bb9ff677ac0
Jason Bukowski <jbukowski@gmail.com>
2019-11-05 12:14:18 -0500

    Monitor for single confirmation before informing cal of new transaction

--- node-btc-mon-service/server.js
+++ node-btc-mon-service/server.js
@@ -23,8 +23,10 @@ const amqp = require('amqplib')
 const connections = require('./lib/connections.js')
 const logger = require('./lib/logger.js')
 
-// Key for the Redis set of all Bitcoin transaction id objects needing to be monitored.
-const BTC_TX_IDS_KEY = 'BTC_Mon:BTCTxIds'
+// Key for the Redis set of all Bitcoin transaction id objects needing to be monitored for first confirmation
+const NEW_BTC_TX_IDS_KEY = 'BTC_Mon:NewBTCTxIds'
+// Key for the Redis set of all Bitcoin transaction id objects needing to be monitored for final confirmation
+const CONFIRMED_BTC_TX_IDS_KEY = 'BTC_Mon:ConfirmedBTCTxIds'
 
 // The merkle tools object for building trees and generating proof paths
 const merkleTools = new MerkleTools()
@@ -38,33 +40,49 @@ let redis = null
 
 let CHECKS_IN_PROGRESS = false
 
-async function consumeBtcTxIdMessageAsync(msg) {
+async function consumeBtcMonMessageAsync(msg) {
   if (msg !== null) {
-    let btcTxIdObjJSON = msg.content.toString()
-
-    try {
-      // add the transaction id to the redis set
-      // Redis is the sole storage mechanism for this data
-      await redis.sadd(BTC_TX_IDS_KEY, btcTxIdObjJSON)
-      amqpChannel.ack(msg)
-    } catch (error) {
-      amqpChannel.nack(msg)
-      logger.error(`${env.RMQ_WORK_IN_BTCMON_QUEUE} : consume message nacked`)
+    // determine the source of the message and handle appropriately
+    switch (msg.properties.type) {
+      case 'newtx':
+        try {
+          // add the transaction id to the redis set
+          // Redis is the sole storage mechanism for this data
+          let newBtcTxIdObjJSON = msg.content.toString()
+          await redis.sadd(NEW_BTC_TX_IDS_KEY, newBtcTxIdObjJSON)
+          amqpChannel.ack(msg)
+        } catch (error) {
+          amqpChannel.nack(msg)
+          logger.error(`${env.RMQ_WORK_IN_BTCMON_QUEUE} : consume message nacked`)
+        }
+        break
+      case 'confirmedtx':
+        try {
+          // add the transaction id to the redis set
+          // Redis is the sole storage mechanism for this data
+          let confirmedBtcTxIdObjJSON = msg.content.toString()
+          await redis.sadd(CONFIRMED_BTC_TX_IDS_KEY, confirmedBtcTxIdObjJSON)
+          amqpChannel.ack(msg)
+        } catch (error) {
+          amqpChannel.nack(msg)
+          logger.error(`${env.RMQ_WORK_IN_BTCMON_QUEUE} : consume message nacked`)
+        }
+        break
+      default:
+        console.error('consumeBtcMonMessageAsync : unknown message type', msg.properties.type)
+        // cannot handle unknown type messages, ack message and do nothing
+        amqpChannel.ack(msg)
     }
   }
 }
 
-// Iterate through all BTCTXIDS objects, checking the confirmation count for each transaction
-// If MIN_BTC_CONFIRMS is reached for a given transaction, retrieve the state data needed
-// to build the proof path from the transaction to the block header merkle root value and
-// return that information to calendar service, ack message.
 let monitorTransactionsAsync = async () => {
   // if the amqp channel is null (closed), processing should not continue, defer to next monitorTransactions call
   if (amqpChannel === null || redis === null) return
 
   CHECKS_IN_PROGRESS = true
-  let btcTxObjJSONArray = await redis.smembers(BTC_TX_IDS_KEY)
-  logger.info(`Btc Tx monitoring check starting for ${btcTxObjJSONArray.length} transaction(s)`)
+  let newBtcTxObjJSONArray = await redis.smembers(NEW_BTC_TX_IDS_KEY)
+  let confirmedBtcTxObjJSONArray = await redis.smembers(CONFIRMED_BTC_TX_IDS_KEY)
 
   let lnd
   try {
@@ -81,31 +99,89 @@ let monitorTransactionsAsync = async () => {
     CHECKS_IN_PROGRESS = false
     return
   }
-
-  for (let btcTxObjJSON of btcTxObjJSONArray) {
-    let btcTxIdObj = JSON.parse(btcTxObjJSON)
+  // Iterate through all new BTCTXIDS objects, checking the confirmation count for each transaction
+  // If a given transaction has been confirmed at least once, send the transaction information
+  // along with the block height of the confirming block to calendar service, ack message.
+  logger.info(`New Btc Tx monitoring check starting for ${newBtcTxObjJSONArray.length} transaction(s)`)
+  for (let newbtcTxObjJSON of newBtcTxObjJSONArray) {
+    let newBtcTxIdObj = JSON.parse(newbtcTxObjJSON)
     try {
       // Get BTC Transaction Stats
       let txStats
       try {
-        txStats = await lnd.getTransactionDataAsync(btcTxIdObj.tx_id)
+        txStats = await lnd.getTransactionDataAsync(newBtcTxIdObj.tx_id)
       } catch (error) {
-        throw new Error(`Could not get stats for transaction ${btcTxIdObj.tx_id}`)
+        throw new Error(`Could not get stats for transaction ${newBtcTxIdObj.tx_id}`)
       }
-      if (txStats.confirmations < env.MIN_BTC_CONFIRMS) {
-        logger.info(`${txStats.txId} not ready : ${txStats.confirmations} of ${env.MIN_BTC_CONFIRMS} confirmations`)
+      if (txStats.confirmations < 1) {
+        logger.info(`${txStats.txId} not yet confirmed`)
         continue
       }
 
-      // if ready, Get BTC Block Stats with Transaction Ids
+      // if confirmed, Get BTC Block info
       let blockStats
       try {
         blockStats = await lnd.getBlockDataAsync(txStats.blockHash)
       } catch (error) {
         throw new Error(`Could not get stats for block ${txStats.blockHash}`)
       }
-      let txIndex = blockStats.tx.indexOf(txStats.txId)
-      if (txIndex === -1) throw new Error(`transaction ${txStats.txId} not found in block ${blockStats.height}`)
+
+      let messageObj = {}
+      messageObj.btctx_id = newBtcTxIdObj.tx_id
+      messageObj.btctx_body = newBtcTxIdObj.tx_body
+      messageObj.btctx_height = blockStats.height
+      try {
+        await amqpChannel.sendToQueue(env.RMQ_WORK_OUT_CAL_QUEUE, Buffer.from(JSON.stringify(messageObj)), {
+          persistent: true,
+          type: 'btcmon_new'
+        })
+        logger.info(`${env.RMQ_WORK_OUT_CAL_QUEUE} : [btcmon] publish message acked : ${messageObj.btctx_id}`)
+      } catch (error) {
+        logger.error(`${env.RMQ_WORK_OUT_CAL_QUEUE} : [btcmon] publish message nacked : ${messageObj.btctx_id}`)
+        throw new Error(error.message)
+      }
+
+      await redis.srem(NEW_BTC_TX_IDS_KEY, newbtcTxObjJSON)
+
+      logger.info(`${newBtcTxIdObj.tx_id} ready with ${txStats.confirmations} confirmations`)
+    } catch (error) {
+      logger.error(`An unexpected error occurred while monitoring : ${error.message}`)
+    }
+  }
+  logger.info(`New Btc Tx monitoring checks complete`)
+
+  // Iterate through all confirmed BTCTXIDS objects, checking the confirmation count for each transaction
+  // If MIN_BTC_CONFIRMS is reached for a given transaction, retrieve the state data needed
+  // to build the proof path from the transaction to the block header merkle root value and
+  // return that information to calendar service, ack message.
+  logger.info(`Confirmed Btc Tx monitoring check starting for ${confirmedBtcTxObjJSONArray.length} transaction(s)`)
+  for (let confirmedBtcTxObjJSON of confirmedBtcTxObjJSONArray) {
+    let confirmedBtcTxIdObj = JSON.parse(confirmedBtcTxObjJSON)
+    try {
+      let txId = confirmedBtcTxIdObj.tx_id
+      let txBlockHeight = confirmedBtcTxIdObj.block_height
+      // Get current BTC block height
+      let confirmCount = 0
+      try {
+        let info = await lnd.getChainInfoAsync()
+        confirmCount = info.topBlockHeight - txBlockHeight + 1
+        if (confirmCount < env.MIN_BTC_CONFIRMS) {
+          logger.info(`${txId} not ready : ${confirmCount} of ${env.MIN_BTC_CONFIRMS} confirmations`)
+          continue
+        }
+      } catch (error) {
+        throw new Error(`Could not retrieve node info`)
+      }
+
+      // if ready, Get BTC Block Stats with Transaction Ids
+      let blockStats
+      try {
+        blockStats = await lnd.getBlockDataAsync(txBlockHeight)
+      } catch (error) {
+        throw new Error(`Could not get stats for block ${txBlockHeight}`)
+      }
+      let txIndex = blockStats.tx.indexOf(txId)
+      if (txIndex === -1) throw new Error(`transaction ${txId} not found in block ${txBlockHeight}`)
       // adjusting for endieness, reverse txids for further processing
       blockStats.tx = blockStats.tx.map(txId =>
         txId
@@ -114,7 +190,7 @@ let monitorTransactionsAsync = async () => {
           .join('')
       )
 
-      if (blockStats.tx.length === 0) throw new Error(`No transactions found in block ${blockStats.height}`)
+      if (blockStats.tx.length === 0) throw new Error(`No transactions found in block ${txBlockHeight}`)
 
       // build BTC merkle tree with txIds
       merkleTools.resetTree()
@@ -131,20 +207,20 @@ let monitorTransactionsAsync = async () => {
         throw new Error(
           `calculated merkle root (${rootValueHex}) does not match block merkle root (${
             blockStats.merkleRoot
-          }) for tx ${txStats.txId}`
+          }) for tx ${txId}`
         )
       // get proof path from tx to block root
       let proofPath = merkleTools.getProof(txIndex)
       // send data back to calendar
       let messageObj = {}
-      messageObj.btctx_id = txStats.txId
+      messageObj.btctx_id = txId
       messageObj.btchead_height = blockStats.height
       messageObj.btchead_root = rootValueHex
       messageObj.path = proofPath
       try {
         await amqpChannel.sendToQueue(env.RMQ_WORK_OUT_CAL_QUEUE, Buffer.from(JSON.stringify(messageObj)), {
           persistent: true,
-          type: 'btcmon'
+          type: 'btcmon_confirmed'
         })
         logger.info(`${env.RMQ_WORK_OUT_CAL_QUEUE} : [btcmon] publish message acked : ${messageObj.btctx_id}`)
       } catch (error) {
@@ -152,15 +228,15 @@ let monitorTransactionsAsync = async () => {
         throw new Error(error.message)
       }
 
-      await redis.srem(BTC_TX_IDS_KEY, btcTxObjJSON)
+      await redis.srem(CONFIRMED_BTC_TX_IDS_KEY, confirmedBtcTxObjJSON)
 
-      logger.info(`${btcTxIdObj.tx_id} ready with ${txStats.confirmations} confirmations`)
+      logger.info(`${confirmedBtcTxIdObj.tx_id} ready with ${confirmCount} confirmations`)
     } catch (error) {
       logger.error(`An unexpected error occurred while monitoring : ${error.message}`)
     }
   }
+  logger.info(`Confirmed Btc Tx monitoring checks complete`)
 
-  logger.info(`Btc Tx monitoring checks complete`)
   CHECKS_IN_PROGRESS = false
 }
 
@@ -199,7 +275,7 @@ async function openRMQConnectionAsync(connectURI) {
     {
       queue: env.RMQ_WORK_IN_BTCMON_QUEUE,
       method: msg => {
-        consumeBtcTxIdMessageAsync(msg)
+        consumeBtcMonMessageAsync(msg)
       }
     },
     chan => {
@@ -255,6 +331,6 @@ module.exports = {
     amqpChannel = chan
   },
   openRMQConnectionAsync: openRMQConnectionAsync,
-  consumeBtcTxIdMessageAsync: consumeBtcTxIdMessageAsync,
+  consumeBtcMonMessageAsync: consumeBtcMonMessageAsync,
   monitorTransactionsAsync: monitorTransactionsAsync
 }
