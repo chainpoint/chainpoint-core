0dea3dfdfbefa3399e6cb304a887ffe162199c96
jacohend <jacob@tierion.com>
2020-01-08 12:56:17 -0500

    fix binary location error on some versions of ubuntu

--- cli/scripts/install_deps.sh
+++ cli/scripts/install_deps.sh
@@ -22,6 +22,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     fi
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose || echo Binary is not at usual location or is already linked
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew install caskroom/cask/brew-cask
