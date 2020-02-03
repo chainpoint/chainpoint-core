64f5387c2afd18240d592d81057e1d5fe8a01b7c
Jacob Henderson <jacobhenderson@Jacobs-MBP.attlocal.net>
2020-02-03 12:56:36 -0500

    convert lnd-mon into secure unlocker daemon only, remove lnd-mon readme, correct go-abci-service readme

--- go-abci-service/README.md
+++ go-abci-service/README.md
@@ -8,9 +8,9 @@ This service module encompasses a Tendermint Core, Chainpoint ABCI app, and vari
 
 ## Operation Modes
 
-When initialized, Tendermint will create Genesis, Config, and Node_Key files in `~/.chainpoint/core/config/node_1`. When Chainpoint-Core is set to `PRIVATE_NETWORK=true` and run immediately after initialization, it will run standalone, without peering with a wider Chainpoint Network. However, when Genesis and Config files are synced to an existing Network via `make init-chain`, Chainpoint-Core will fast-sync with the Network and become a full-node capable of sharing Bitcoin anchoring responsibilities.
+When initialized, Tendermint will create Genesis, Config, and Node_Key files in `~/.chainpoint/core/config/node_1`. All other data, including ECDSA, redis, and postgresql files, can be found in `~/.chainpoint/core/data`.
 
-A Tendermint full-node can become a validator, and share greater responsibilities such as receiving hashes from Nodes, generating proofs, Minting Rewards for well-behaved Cores and Nodes, and injecting NIST Beacon entropy into the blockchain.
+A Tendermint full-node can become a validator, and share greater responsibilities such as confirming new Cores, generating proofs, and injecting NIST Beacon entropy into the blockchain.
 
 ## Deeper Dive
 
@@ -23,13 +23,11 @@ Every block epoch (60 seconds by default), the ABCI application is set to perfor
 - Elect a leader to send a NIST Beacon Entropy transaction to the blockchain.
 - Monitor for public keys from new Cores, which are broadcast via a JWK message.
 - Monitor sync status with the rest of the Network (and shutdown critical functions if not synced yet).
-- Receive information about new Nodes and Cores from the Chainpoint Registry ethereum smart contract.
 
 Every anchor epoch (60 minutes by default), the ABCI application is set to perform the following anchor/reward functions:
 
-- Elect a leader to anchor all hashes received since last anchor epoch by broadcasting their Merkle Root to Bitcoin via `btc-tx-service`. The resulting Bitcoin TX ID is placed in a BTC-A transaction and submitted to the Calendar.
+- Elect a leader to anchor all hashes received since last anchor epoch by broadcasting their Merkle Root to Bitcoin via the `lnd` service. The resulting Bitcoin TX ID is placed in a BTC-A transaction and submitted to the Calendar.
 - Monitor for the confirmation of a successful anchor to Bitcoin via the `btc-mon-service`. The resulting Bitcoin header info containing the anchor is placed in a BTC-C transaction and submitted to the Calendar.
-- Elect a leader to audit Nodes, then reward good behavior every 24 hours. Upon successful reward, a NODE-MINT transaction is broadcast to the Calendar.
 
 At any time, the ABCI application may:
 
--- node-lnd-mon-service/server.js
+++ node-lnd-mon-service/server.js
@@ -16,170 +16,30 @@
 
 // load all environment variables into env object
 const env = require('./lib/parse-env.js')('lnd-mon')
-
-const connections = require('./lib/connections.js')
 const logger = require('./lib/logger.js')
-const utils = require('./lib/utils.js')
 const lndClient = require('lnrpc-node-client')
-const fs = require('fs')
-
 const LND_SOCKET = env.LND_SOCKET
 const LND_CERTPATH = `/root/.lnd/tls.cert`
-const LND_MACAROONPATH = `/root/.lnd/data/chain/bitcoin/${env.NETWORK}/admin.macaroon`
-
-const LAST_KNOWN_INVOICE_INDEX_KEY = 'LastKnownInvoiceIndex'
-
-// This value is set once the connection has been established
-let redis = null
-
-/**
- * Opens a Redis connection
- *
- * @param {string} redisURI - The connection string for the Redis instance, an Redis URI
- */
-function openRedisConnection(redisURIs) {
-  connections.openRedisConnection(
-    redisURIs,
-    newRedis => {
-      redis = newRedis
-    },
-    () => {
-      redis = null
-      setTimeout(() => {
-        openRedisConnection(redisURIs)
-      }, 5000)
-    }
-  )
-}
-
-async function startInvoiceMonitoring() {
-  let subscriptionEstablished = false
-  if (!fs.existsSync(LND_CERTPATH)) {
-    throw new Error(`LND TLS Cert not yet generated, restarting...`)
-  }
-  while (!subscriptionEstablished) {
-    try {
-      // establish a connection to lnd
-      try {
-        await ensureLndNodeClientWalletUnlockedAsync()
-        lndClient.setCredentials(LND_SOCKET, LND_MACAROONPATH, LND_CERTPATH)
-      } catch (error) {
-        throw new Error(`Cannot establish active LND connection : ${error.message}`)
-      }
-      // retrieve the add_index of the most recently handled invoice
-      let lastKnownInvoiceIndex = await getLastKnownInvoiceIndexAsync()
-      // starting listening for and handle new invoice activity
-      await establishInvoiceSubscriptionAsync(lastKnownInvoiceIndex)
-      subscriptionEstablished = true
-    } catch (error) {
-      // catch errors when attempting to connect and establish invoice subscription
-      logger.error(`Invoice monitoring : ${error.message} : Retrying in 5 seconds...`)
-      await utils.sleepAsync(5000)
-    }
-  }
-}
 
 async function ensureLndNodeClientWalletUnlockedAsync() {
   lndClient.setTls(LND_SOCKET, LND_CERTPATH)
   let unlocker = lndClient.unlocker()
   try {
     await unlocker.unlockWalletAsync({ wallet_password: env.HOT_WALLET_PASS, recovery_window: 500 })
+    logger.info("Wallet unlocked")
   } catch (error) {
-    if (error.code === 12) return // already unlocked
-  }
-  throw new Error(`Unable to unlock wallet`)
-}
-
-async function establishInvoiceSubscriptionAsync(addIndex) {
-  try {
-    let invoiceSubscription = lndClient.lightning().subscribeInvoices({ add_index: addIndex })
-    invoiceSubscription.on('data', async invoice => {
-      let invoiceAddIndex = await processInvoiceBatchAsync([invoice])
-      await updateLastKnownInvoiceIndexAsync(invoiceAddIndex)
-    })
-    invoiceSubscription.on('status', function(status) {
-      logger.warn(`LND invoice subscription status has changed (${status.code}) ${status.details}`)
-    })
-    invoiceSubscription.on('end', function() {
-      logger.error(`The LND invoice subscription has unexpectedly ended`)
-      setTimeout(startInvoiceMonitoring, 1000)
-    })
-    logger.info('LND invoice subscription has been established')
-  } catch (error) {
-    throw new Error(`Unable to establish LND invoice subscription : ${error.message}`)
-  }
-}
-
-async function getLastKnownInvoiceIndexAsync() {
-  // return the add_index value of the newest invoice processed
-  let lastKnownInvoiceIndex
-  try {
-    lastKnownInvoiceIndex = parseInt(await redis.get(LAST_KNOWN_INVOICE_INDEX_KEY))
-    if (isNaN(lastKnownInvoiceIndex)) lastKnownInvoiceIndex = 0 // no value set yet, do not retrieve past invoices
-  } catch (error) {
-    logger.warn(`Unable to retrieve last known invoice index, skipping : ${error.message}`)
-    lastKnownInvoiceIndex = 0
-  }
-  return lastKnownInvoiceIndex
-}
-
-async function updateLastKnownInvoiceIndexAsync(newInvoiceAddIndex) {
-  try {
-    // retrieve the current last known invoice index value
-    // update this value only if the new value is greater than the current value
-    let lastKnownIndex = await getLastKnownInvoiceIndexAsync()
-    if (newInvoiceAddIndex > lastKnownIndex) await redis.set(LAST_KNOWN_INVOICE_INDEX_KEY, newInvoiceAddIndex)
-  } catch (error) {
-    logger.error(`Unable to update LAST_KNOWN_INVOICE_INDEX_KEY : value = ${newInvoiceAddIndex} : ${error.message}`)
-  }
-}
-
-async function processInvoiceBatchAsync(invoices) {
-  let invoiceRedisOps = invoices
-    .map(invoice => {
-      if (!invoice.settled) {
-        // This invoice was created but not yet paid
-        logger.info(`Invoice generated : ${invoice.memo} : index ${invoice.add_index}, ${invoice.settle_index}`)
-        return null
-      } else {
-        // This invoice has been paid
-        logger.info(`Invoice paid : ${invoice.memo} : index ${invoice.add_index}, ${invoice.settle_index}`)
-        // Add a key to redis indicating the payment has been made
-        // With this key added, the invoice id can be used to submit a hash one time
-        let invoiceId = invoice.memo.split(':')[1]
-        let paidInvoiceKey = `PaidSubmitHashInvoiceId:${invoiceId}`
-        return ['set', paidInvoiceKey, '1', 'EX', '120']
-      }
-    })
-    .filter(invoice => invoice !== null)
-  // process all the redis oprations and return the most recently processed invoice add_index
-  let lastInvoiceAddIndex = invoices[invoices.length - 1].add_index
-  try {
-    await redis.multi(invoiceRedisOps).exec()
-    return lastInvoiceAddIndex
-  } catch (error) {
-    logger.error(
-      `Redis MULTI SET error : error setting item batch ending with invoice index = ${lastInvoiceAddIndex} : ${
-        error.message
-      }`
-    )
-    return null
+    if (error.code === 12) {
+      logger.info("Wallet already unlocked")
+      return // already unlocked
+    }
+    logger.error(`Unable to unlock wallet, retrying in 10 seconds...`)
   }
 }
 
 // process all steps need to start the application
 async function start() {
   if (env.NODE_ENV === 'test') return
-  try {
-    // init Redis
-    openRedisConnection(env.REDIS_CONNECT_URIS)
-    // init listening for lnd invoice update events
-    await startInvoiceMonitoring()
-    logger.info(`Startup completed successfully`)
-  } catch (error) {
-    logger.error(`An error has occurred on startup : ${error.message}`)
-    process.exit(1)
-  }
+  setInterval(ensureLndNodeClientWalletUnlockedAsync, 10000)
 }
 
 // get the whole show started
