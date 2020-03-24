2196876a24c9d5366ad70ff62fc3b097754c6ec1
Jacob Henderson <jacobhenderson@Jacobs-MBP.attlocal.net>
2020-03-24 10:44:07 -0400

    allow testnet a shortcut for testing

--- go-abci-service/abci/transactions.go
+++ go-abci-service/abci/transactions.go
@@ -50,7 +50,7 @@ func (app *AnchorApplication) validateTx(rawTx []byte) types2.ResponseCheckTx {
 		}
 		goodCandidateForValidator := false
 		if _, record, err := validation.GetValidationRecord(addr, app.state); err != nil {
-			goodCandidateForValidator = record.ConfirmedAnchors > SUCCESSFUL_ANCHOR_CRITERIA
+			goodCandidateForValidator = (record.ConfirmedAnchors > SUCCESSFUL_ANCHOR_CRITERIA || app.config.BitcoinNetwork == "testnet")
 		}
 		app.logger.Info("VAL tx is from a validator? ", "isVal", isVal)
 		app.LogError(err)
