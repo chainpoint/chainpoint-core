f7de052569241ace8fd6176c3881b849f9a484ec
Jacob Henderson <jacob@tierion.com>
2020-03-27 11:43:11 -0400

    Leader fix (#28)
    * add naive, unused implementation of leader election that doesn't use the p2p layer
    
    * add leader election candidate list filtered by last n successful nist submissions
    
    * permanently switch over to new leader election method
    
    Co-authored-by: Jacob Henderson <jacobhenderson@Jacobs-MBP.attlocal.net>

--- go-abci-service/abci/anchor.go
+++ go-abci-service/abci/anchor.go
@@ -53,11 +53,11 @@ func (app *AnchorApplication) AnchorBTC(startTxRange int64, endTxRange int64) er
 	app.logger.Debug(fmt.Sprintf("starting scheduled anchor period for tx ranges %d to %d", startTxRange, endTxRange))
 
 	// elect leader to do the actual anchoring
-	iAmLeader, leaderIDs := app.ElectLeader(1)
+	iAmLeader, leaderIDs := app.ElectChainContributorAsLeader(1)
 	if len(leaderIDs) == 0 {
 		return errors.New("Leader election error")
 	}
-	app.logger.Debug(fmt.Sprintf("Leaders: %v", leaderIDs))
+	app.logger.Info(fmt.Sprintf("Leaders: %v", leaderIDs))
 
 	// Get CAL transactions between the latest BTCA tx and the current latest tx
 	txLeaves, err := app.getCalTxRange(startTxRange, endTxRange)
@@ -197,7 +197,7 @@ func (app *AnchorApplication) ConsumeBtcMonMsg(msg amqp.Delivery) error {
 	conf_found := false
 	for !time.Now().After(deadline) && !conf_found {
 		// Broadcast the confirmation message with metadata
-		amLeader, _ := app.ElectValidator(1)
+		amLeader, _ := app.ElectValidatorAsLeader(1)
 		if amLeader {
 			result, err := app.rpc.BroadcastTxWithMeta("BTC-C", btcMonObj.BtcHeadRoot, 2, time.Now().Unix(), app.ID, anchoringCoreID+"|"+btcMonObj.BtcTxID, &app.config.ECPrivateKey)
 			app.LogError(err)
