fe07e3fa33d0cce6e71b4ddd1e42c84b96e72635
jacohend <jacob@tierion.com>
2021-09-15 11:17:12 -0400

    delete docker and modify installation tooling

--- cli/scripts/install_deps.sh
+++ cli/scripts/install_deps.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-if [ -x "$(command -v docker)" ]; then
-    echo "Docker already installed"
-else
-    echo "Install docker"
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    bash get-docker.sh
-    sudo usermod -aG docker $USER
-fi
+install_deps(){
+  echo "Running installation as"
+  echo "Username: $USER"
+  echo "    EUID: $EUID"
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt-get -qq update -y
-    sudo apt-get -qq install -y apt-utils curl dirmngr apt-transport-https lsb-release ca-certificates g++
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-    sudo apt-get -qq install -y docker-compose git make jq nodejs openssl
-    sudo apt-get -qq install -y npm || echo "NPM already installed with nodejs"
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    if ! [ -x "$(command -v yarn)" ]; then
-        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-        sudo apt-get update -y && sudo apt-get install -y --no-install-recommends yarn
-    fi
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose || echo Binary is not at usual location or is already linked
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install caskroom/cask/brew-cask
-    brew cask install docker-toolbox
-    brew install jq
-    brew install homebrew/core/make
-    brew install git
-    brew install node
-    brew install yarn
-    brew install openssl
-fi
+    sudo bash -c "apt-get update -y && apt-get install -y wget build-essential libsnappy-dev"
 
-yarn
+    wget https://github.com/google/leveldb/archive/v1.20.tar.gz && \
+    tar -zxvf v1.20.tar.gz && \
+    cd leveldb-1.20/ && \
+    make && \
+    cp -r out-static/lib* out-shared/lib* /usr/local/lib/ && \
+    cd include/ && \
+    cp -r leveldb /usr/local/include/ && \
+    ldconfig && \
+    rm -f v1.20.tar.gz
+
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install snappy wget
+    cd /usr/local/lib/
+    ln -s /usr/local/Cellar/snappy/1.1.1/lib/libsnappy.1.dylib
+    ln -s /usr/local/Cellar/snappy/1.1.1/lib/libsnappy.a
+    ln -s /usr/local/Cellar/snappy/1.1.1/lib/libsnappy.dylib
+    update_dyld_shared_cache
+
+    wget https://github.com/google/leveldb/archive/v1.20.tar.gz && \
+    tar -zxvf v1.20.tar.gz && \
+    cd leveldb-1.20/ && \
+    make && \
+    cp -r out-static/lib* out-shared/lib* /usr/local/lib/ && \
+    cd include/ && \
+    cp -r leveldb /usr/local/include/ && \
+    update_dyld_shared_cache && \
+    rm -f v1.20.tar.gz
+  fi
+}
+
+sudo bash -c "$(declare -f install_deps); install_deps"
