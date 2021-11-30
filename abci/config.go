4cc885dc9aa9a6e5a87136e59443281c89a4f769
jacohend <jacob@tierion.com>
2021-11-30 12:50:00 -0500

    fix updateStake default value

--- abci/config.go
+++ abci/config.go
@@ -62,7 +62,7 @@ func InitConfig(home string) types.AnchorConfig {
 	flag.Float64Var(&feeMultiplier, "btc_fee_multiplier", 2.2, "multiply anchoring fee by this constant when mempool is congested")
 	flag.IntVar(&feeInterval, "fee_interval", 10, "interval in minutes to check for new bitcoin tx fee")
 	flag.IntVar(&stakePerCore, "stake_per_core", 1000000, "minimum amount staked per channel to permit the addition of a Core")
-	flag.IntVar(&updateStake, "update_stake", 1000000, "a validator may change this value to adjust the stake_per_core")
+	flag.IntVar(&updateStake, "update_stake", 0, "a validator may change this value to adjust the stake_per_core")
 	flag.StringVar(&sessionSecret, "session_secret", "", "mutual LSAT macaroon secret for cores and gateways")
 	flag.StringVar(&tmServer, "tendermint_host", "127.0.0.1", "tendermint api url")
 	flag.StringVar(&tmPort, "tendermint_port", "26657", "tendermint api port")
